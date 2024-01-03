bf7f4e5bafa4f2bb83f9ae3521550a6323a3faa8
Roman Havriluk <pomahtik_inc@outlook.com>
2024-01-03 10:26:27 +0200

    namespace change

--- Creational_Paterns/Factory.cs
+++ Creational_Paterns/Factory.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace patternsSamples.Creational_Paterns
+namespace patternsSamples.Creational_Paterns.Factory
 {
     internal class Factory
     {
