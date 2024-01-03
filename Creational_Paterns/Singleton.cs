4b953d88665b31aea6c3628fe0b8f1a71e728654
Roman Havriluk <pomahtik_inc@outlook.com>
2024-01-03 12:16:35 +0200

    singleton as creational patern

---
+++ Creational_Paterns/Singleton.cs
@@ -0,0 +1,19 @@
+﻿// Задача: тут я задачу придумати не зміг, тому просто приклад реалізації цього патерну.
+// Рішення: створення класу, який створюється лише один раз, і при кожній наступній спробі створити новий клас, повертає той що був створений першим.
+
+namespace patternsSamples.Creational_Paterns
+{
+    internal class Singleton
+    {
+        private static Singleton? instance;
+        private Singleton() 
+        { 
+            instance = this;
+        }
+
+        public static Singleton GetSingleton()
+        {
+            return instance ?? new Singleton();
+        }
+    }
+}
