2f7a2e51b040c3518e0eadbb38e7376a7342887d
Roman Havriluk <pomahtik_inc@outlook.com>
2024-01-03 12:08:41 +0200

    Prototype pattern finished

--- Creational_Paterns/Prototype.cs
+++ Creational_Paterns/Prototype.cs
@@ -1,9 +1,18 @@
 ﻿// Задача: тепер необхідна можливість виготовлення повної копії стільців з певного шаблону.
 // Рішення: патерн Prototype, який дозволяє створювати копію об'єкту.
 // Примітка 1: в випадку з C#, це означає реалізацію інтерфейсу ICloneable, в загальному випадку, означає створення такого інтерфейсу окремо.
+// Примітка 2: конструктор копіювання в данному випадку не підходить.
+
 namespace patternsSamples.Creational_Paterns.Prototype
 {
-    internal class Prototype
+    internal class Chair : ICloneable
     {
+        int legs;
+        string name;
+        bool back;
+        public object Clone()
+        {
+            return new Chair() { legs = legs, name = name, back = back };    
+        }
     }
 }
