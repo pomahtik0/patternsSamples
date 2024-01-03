8ffe3c03d64628f95d745f80109aa2c620d4b6b7
Roman Havriluk <pomahtik_inc@outlook.com>
2024-01-03 11:55:45 +0200

    applied builder pattern

--- Creational_Paterns/Builder.cs
+++ Creational_Paterns/Builder.cs
@@ -2,7 +2,21 @@
 // Рішення: вирішимо подібну задачу за допомогою патерну Builder, що передбачає створення класу який збирає наш "стілець", по крокам (не всі з яких є обов'язкові).
 namespace patternsSamples.Creational_Paterns.Builder
 {
+    internal class Chair
+    {
+        public int NumberOfLegs { get; set; }
+        public bool HasASit { get; set; }
+        public bool HasABack { get; set; }
+        public bool HasAnArmrest { get; set; }
+    }
     internal class Builder
     {
+        Chair chair = new Chair();
+        public void PutLegs(int numberOfLegs) => chair.NumberOfLegs = numberOfLegs;
+        public void PutASit() => chair.HasASit = true;
+        public void PutABack() => chair.HasABack = true;
+        public void PutAnArmrest() => chair.HasAnArmrest = true;
+        public Chair GetCreatedChair() => chair;
+        public void Reset() => chair = new Chair();
     }
 }
