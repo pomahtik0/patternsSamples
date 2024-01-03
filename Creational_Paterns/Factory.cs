c6d4c798ad765586a20cb13aaa96a4cd019c3424
Roman Havriluk <pomahtik_inc@outlook.com>
2024-01-03 10:42:54 +0200

    created simple factory using factory pattern

--- Creational_Paterns/Factory.cs
+++ Creational_Paterns/Factory.cs
@@ -6,7 +6,41 @@ using System.Threading.Tasks;
 
 namespace patternsSamples.Creational_Paterns.Factory
 {
-    internal class Factory
+    internal interface IFurniture
     {
+        public void Description();
+    }
+    internal class Chair : IFurniture
+    {
+        public void Description()
+        {
+            Console.WriteLine("It's a chair");
+        }
+    }
+    internal class Table : IFurniture
+    {
+        public void Description()
+        {
+            Console.WriteLine("It's a table");
+        }
+    }
+
+    internal abstract class Factory
+    {
+        public abstract IFurniture CreateProduct();
+    }
+    internal class FactoryOfChairs : Factory 
+    {
+        public override IFurniture CreateProduct()
+        {
+            return new Chair();
+        }
+    }
+    internal class FactoryOfTables : Factory 
+    {
+        public override IFurniture CreateProduct()
+        {
+            return new Table();
+        }
     }
 }
