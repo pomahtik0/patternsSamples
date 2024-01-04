449a94e2718fc97349482a9cc7da7255ca95a34d
Roman Havriluk <pomahtik_inc@outlook.com>
2024-01-04 10:23:08 +0200

    styles created

---
+++ Structural_Paterns/Bridge.cs
@@ -0,0 +1,29 @@
+﻿// Задача: уявимо ту ж задачу, що і з абстрактною фабрикою, потрібно аби була можливість виготовляти меблі в різному стилі, але при цьому потрібно максимально спростити додавання нового виду меблів, або нових стилів.
+// Рішення: використання патерну Bridge
+namespace patternsSamples.Structural_Paterns.Bridge
+{
+    #region Styles
+    internal interface IStyle
+    {
+        string GetStyle();
+    }
+    internal class Modern : IStyle
+    {
+        public string GetStyle()
+        {
+            return "Modern";
+        }
+    }
+    internal class Retro : IStyle
+    {
+        public string GetStyle()
+        {
+            return "Retro";
+        }
+    }
+    #endregion
+    internal interface IFurniture
+    {
+        public IStyle CurrentStyle { get; set; }
+    }
+}
